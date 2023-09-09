d1662ef271ce907c02c00d141498156ffee78b8c
yuo906 <140991318+yuo906@users.noreply.github.com>
2023-09-09 11:50:57 +0800

    checkout Order Detail blade 切版

--- resources/views/checkout/checkout_order_detail.blade.php
+++ resources/views/checkout/checkout_order_detail.blade.php
@@ -1,6 +1,60 @@
 @extends('templates.template_website')
 @section('main')
-    <main class="container my-5">
-        
+    <main class="container my-1">
+        {{-- 分頁標籤 --}}
+        <div>
+            <span class="text-success">Home</span>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;
+            <span class="text-success">Shop</span>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;
+            <span class="text-muted">Shop Checkout</span>
+        </div>
+
+        {{-- 標題 --}}
+        <div class="title py-4 mb-4 px-2 w-100">
+            <h2 class="fw-medium w-50 bold">Checkout</h2>
+            <div class="text-muted">Already have an account? Click here to
+                <a href="" class="text-success">Sign in</a>.
+            </div>
+        </div>
+
+        {{-- 商品列表 --}}
+        <div>
+            <div class="order-details-head border rounded-top">
+                <div class="p-3">Order Details</div>
+            </div>
+            <div class="order-details-body">
+                <div class="p-3 d-flex justify-content-between align-items-center border">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <img style="width:150px;" src="{{ asset('image/product-img-1.jpg') }}" alt="">
+                        </div>
+                        <div>
+                            <div class="product-name">Cadbury 5 Star Chocolate</div>
+                            <div class="product-desc text-muted">1kg</div>
+                        </div>
+                    </div>
+                    <div>$15.00</div>
+                </div>
+                <div class="p-3 d-flex justify-content-between align-items-center border">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <img style="width:150px;" src="{{ asset('image/product-img-2.jpg') }}" alt="">
+                        </div>
+                        <div>
+                            <div class="product-name">Cadbury 5 Star Chocolate</div>
+                            <div class="product-desc text-muted">1kg</div>
+                        </div>
+                    </div>
+                    <div>$15.00</div>
+                </div>
+            </div>
+            <div class="order-details-footer d-flex justify-content-between align-items-center p-3 border rounded-bottom">
+                <div>Subtotal</div>
+                <div>$30.00</div>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-end py-3">
+            <button class="btn btn-success">Next</button>
+        </div>
     </main>
 @endsection
